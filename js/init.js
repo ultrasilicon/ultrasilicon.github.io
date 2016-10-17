69f78ab3fe433dc6cb0109b36303e875f7d63e2d
Tim Z <zhenghanecho@gmail.com>
2016-10-17 15:44:49 +0800

    Update

---
+++ js/init.js
@@ -0,0 +1,10 @@
+$('.carousel.carousel-slider').carousel({
+    full_width: true
+});
+$('.carousel').carousel('next');
+$('.carousel').carousel('next', 3); // Move next n times.
+// Previous slide
+$('.carousel').carousel('prev');
+$('.carousel').carousel('prev', 4); // Move prev n times.
+// Set to nth slide
+$('.carousel').carousel('set', 4);
